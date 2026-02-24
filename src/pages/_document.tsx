303b4d41ffb87f773883dc7171dfd7d744240933
katom826 <katom826@gmail.com>
2026-02-25 00:56:03 +0900

    ファビコンを設定2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link
+          rel="icon"
+          href="/Chime-pomodoro/favicon.svg"
+          type="image/svg+xml"
+        />
       </Head>
       <body className="antialiased">
         <Main />
