1454f7ec3d6ede0c31e92b572800de3ea70de4ee
katom826 <katom826@gmail.com>
2026-02-25 00:48:41 +0900

    ファビコンを設定

--- out/_next/static/chunks/f8ed7f3a8b1a90c6.js
+++ out/_next/static/chunks/f8ed7f3a8b1a90c6.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/246d31196d81123a.js",
   "static/chunks/fa41eadacb6b067d.js",
-  "static/chunks/9759fe6487a78c29.css",
-  "static/chunks/turbopack-c059ce4726605e83.js"
+  "static/chunks/3e390c5a6fe81592.css",
+  "static/chunks/turbopack-9c3d2a24ede66735.js"
 ])
